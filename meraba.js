93fa8f962b9f5e32bfb9b531d5fa5200b1e3bc86
ali5832 <158829748+ali5832@users.noreply.github.com>
2024-02-04 08:55:49 -0800

    Add files via upload

---
+++ meraba.js
@@ -0,0 +1,10 @@
+let r = prompt()
+for(let i = 0; i<r.length; i++) {
+    if (r.length % 2 === 0) {
+        alert("Daxil etdiyiniz cutdur")
+        break
+    }else {
+        alert ("Tekdir cut daxil edin")
+        break
+    }
+}
